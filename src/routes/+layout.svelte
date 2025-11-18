a8cffd5ee6e37cc57723c491a8b35ec27a853835
André devkcud Albanese Junior <patommmmm@proton.me>
2025-11-18 13:08:35 -0300

    refactor(layout): remove old navbar and move change user to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
     import { getVersion } from '@tauri-apps/api/app';
     import { getCurrentWindow } from '@tauri-apps/api/window';
     import { goto } from '$app/navigation';
-    import { browser } from '$app/environment';
     import 'iconify-icon';
     import '../app.css';
     import Button from '$lib/components/Button.svelte';
@@ -15,25 +14,29 @@
     function logout() {
         goto('/');
     }
-
-    function previousPage() {
-        if (browser) history.back();
-    }
-
-    function nextPage() {
-        if (browser) history.forward();
-    }
 </script>
 
 <header
-    class="fixed top-0 right-0 left-0 z-50 grid h-10 grid-cols-3 items-center gap-2 px-4 select-none"
+    class="fixed inset-x-0 top-0 z-50 grid h-10 grid-cols-3 items-center gap-2 px-3 select-none"
     data-tauri-drag-region
 >
-    <Title size="sm" colored bold>
-        {#await window.title() then title}
-            {title}
-        {/await}
-    </Title>
+    <div class="flex items-center gap-1" data-tauri-drag-region>
+        <Title size="sm" colored bold>
+            {#await window.title() then title}
+                {title}
+            {/await}
+        </Title>
+
+        <Button
+            icon="mdi:users"
+            size="xs"
+            layout="circle"
+            color="ghost"
+            tooltip="Change users"
+            tooltipOrientation="bottom"
+            onclick={logout}
+        />
+    </div>
 
     <div data-tauri-drag-region></div>
 
@@ -62,36 +65,11 @@
     </section>
 </header>
 
-<nav class="navbar fixed top-8 z-40 rounded-2xl">
-    <section class="flex items-center gap-2">
-        <Button icon="mdi:users" size="sm" layout="square" onclick={logout} />
-
-        <div class="join">
-            <Button
-                icon="mdi:arrow-left"
-                size="sm"
-                layout="square"
-                onclick={previousPage}
-                isJoinItem
-            />
-            <Button
-                icon="mdi:arrow-right"
-                size="sm"
-                layout="square"
-                onclick={nextPage}
-                isJoinItem
-            />
-        </div>
-    </section>
-
-    <div class="grow"></div>
-</nav>
-
 <div class="h-screen overflow-auto pt-10">
     {@render children()}
 </div>
 
-<p class="fixed bottom-2 right-4 text-xs opacity-30 select-none">
+<p class="fixed right-4 bottom-2 text-xs opacity-30 select-none">
     {#await getVersion() then version}
         v{version}
     {/await}
